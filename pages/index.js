b159a9a17e54dcdb2eeb7b7f72482ee9014cff9c
Maksym <maksym.sh@upplabs.com>
2021-03-07 18:28:56 +0200

    first-get-static-props

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-function HomePage(props) {
+function HomePage({ products }) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map(({ id, name }) => (
+        <li key={id}>{name}</li>
+      ))}
     </ul>
   );
 }
 
 export default HomePage;
+
+export const getStaticProps = async () => {
+  // executed on server
+  return {
+    props: {
+      products: [
+        {
+          id: 1,
+          name: 'Product 1'
+        }
+      ]
+    }
+  };
+};
