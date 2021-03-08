389fcaa7f5648df5d7be110a2f5a7ba83ee9f3d1
Maksym <maksym.sh@upplabs.com>
2021-03-08 18:12:29 +0200

    dynamic-paths

--- pages/[pid].js
+++ pages/[pid].js
@@ -2,6 +2,11 @@ import path from 'path';
 import fs from 'fs/promises';
 
 const ProductDetail = ({ product }) => {
+  // when fallback set to true, data has to be fetched and it does not happens instantly
+  if (!product) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <h1>{product.title}</h1>
@@ -10,11 +15,16 @@ const ProductDetail = ({ product }) => {
   );
 };
 
-export const getStaticProps = async ({ params }) => {
+const getData = async () => {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json'); // cwd current working directory
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data;
+};
+
+export const getStaticProps = async ({ params }) => {
+  const data = await getData();
   const product = data.products.find(elem => elem.id === params.pid);
 
   return {
@@ -26,20 +36,32 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export const getStaticPaths = async () => {
+  const data = await getData();
+
+  const ids = data.products.map(({ id }) => id);
+
+  const paths = ids.map(id => ({
+    params: {
+      pid: id
+    }
+  }));
+
   return {
-    paths: [
-      {
-        params: { pid: 'p1' }
-      },
-      {
-        params: { pid: 'p2' }
-      },
-      {
-        params: { pid: 'p3' }
-      }
-    ],
-    fallback: false
+    paths: paths,
+    // {
+    //   params: { pid: 'p2' }
+    // },
+    // {
+    //   params: { pid: 'p3' }
+    // }
+    // ],
+    // fallback: true, // allows visiting pages which are not pregenerated
+    fallback: 'blocking'
   };
+
+  // fallback: false --all paths has to be included
+  // fallback: true --allows visiting pages which are not pregenerated, but props need to be checked (first time they are empty)
+  // fallback: 'blocking' --allows visiting pages which are not pregenerated, but page is fully rendered on server, so no props check is needed
 };
 
 export default ProductDetail;
